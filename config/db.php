874d03fd8e84328067b49dcc0e54961bdf101e67
Ifeanyi Favour <ifeanyifavour041@gmail.com>
2026-05-25 23:16:38 +0100

    modified

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ $user = "root";
 $password = "";
 $database = "hr_suite_db";
 
-$conn = mysqli_connect($host, $user, $password, $database);
+$conn = mysqli_connect(mysql.railway.internal, root, QQTNGduqoNDquPCINkcVtfyujjLNapDE, railway, 3306);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
